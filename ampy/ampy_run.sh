0e54828584d61166de7abf3a16acff4714958cdf
@ShrimpingIt Studio Laptop account <shrimpingit_studio_laptop@cefn.com>
2016-12-26 12:07:39 +0000

    Modified ampy scripts to permit loading of resources relative to script location. Centralised clean and release logic

--- ampy/ampy_run.sh
+++ ampy/ampy_run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-killall screen
-ampy --port /dev/ttyUSB0 run `dirname $0`/../regimes/blank_display/main.py
-ampy --port /dev/ttyUSB0 run ./main.py
+MAIN=$1
+CURRENT=`dirname $0`
+AMPY="ampy --port /dev/ttyUSB0"
+"${CURRENT}"/ampy_clean.sh
+# run the file
+"${AMPY}" run "${MAIN}"
